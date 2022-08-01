97bc2a51504e5fa11bcc75fb5b7446fa8d6f7e88
Sidney Pang <sidneypang@hotmail.co.uk>
2022-08-01 22:24:10 +0000

    add test cases for user model, add authentication class in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, uniqueness: {case_sensitive: true}
+
+  validates :name, :email, presence: true
+
+  validates :password, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials (email, password)
+    user = User.where(['email = ?', email.strip.downcase]).first
+
+    if user && user.authenticate(password.strip)
+      return user
+    else 
+      return nil
+    end
+  end
+
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+
+    it 'is valid when validations all pass' do
+      @user = User.create name: "Test", email: "Test@gmail.com", password: '123456789', password_confirmation: '123456789'
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid when passwords are diffferent' do
+      @user = User.create name: "Test", email: "Test@gmail.com", password: '123', password_confirmation: '1234'
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when passwords not filled in' do
+      @user = User.create name: "Test", email: "Test@gmail.com", password: nil, password_confirmation: '1234'
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when email exists' do
+      @user = User.create name: "Test", email: "Test@gmail.com", password: '123', password_confirmation: '123'
+      @user2 = User.create name: "Test", email: "Test@gmail.com", password: '123', password_confirmation: '123'
+
+      expect(@user2).to_not be_valid
+    end
+
+    it 'is not valid when email exists, for case sensitive scenario' do
+      @user = User.create name: "Test", email: "Test@gmail.com", password: '123', password_confirmation: '123'
+      @user2 = User.create name: "Test", email: "test@gmail.com", password: '123', password_confirmation: '123'
+      
+      expect(@user2).to_not be_valid
+    end
+
+    it 'is not valid when name is empty' do
+      @user = User.create name: nil, email: "Test@gmail.com", password: '123', password_confirmation: '123'
+      
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when email is empty' do
+      @user = User.create name: 'Test', email: nil, password: '123', password_confirmation: '123'
+      
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid when password is less than 8 characters' do
+      @user = User.create name: 'Test', email: nil, password: '123', password_confirmation: '123'
+      
+      expect(@user).to_not be_valid
+    end
+
+    it 'is valid when password has minimum 8 characters' do
+      @user = User.create name: 'Test', email: nil, password: '12345678', password_confirmation: '12345678'
+      
+      expect(@user).to_not be_valid
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+
+    before (:each) do
+      @user = User.create name: 'Test', email: "first@gmail.com", password: '12345678', password_confirmation: '12345678'
+    end
+
+    it 'should not be nil if user exists' do
+      @login = User.authenticate_with_credentials('first@gmail.com', '12345678')
+      
+      expect(@login).to_not be_nil
+    end
+
+    it 'should be nil if user does not exist' do
+      @login = User.authenticate_with_credentials('second@gmail.com', '12345678')
+      
+      expect(@login).to be_nil
+    end
+
+    it 'should not be nil if user exists even with white space' do
+      @login = User.authenticate_with_credentials('first@gmail.com      ', '12345678')
+      
+      expect(@login).to_not be_nil
+    end
+
+    it 'should not be nil if user exists and entered different cases' do
+      @login = User.authenticate_with_credentials('First@gmail.com      ', '12345678')
+      
+      expect(@login).to_not be_nil
+    end
+
+
+    
+  end
+
+end
